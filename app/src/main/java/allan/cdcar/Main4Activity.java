841871b3a87496cba81959f1b9756c1a0b2f91bd
kiki4228 <john410500198@gmail.com>
2016-10-18 17:18:07 +0800

    V2

--- app/src/main/java/allan/cdcar/Main4Activity.java
+++ app/src/main/java/allan/cdcar/Main4Activity.java
@@ -1,7 +1,9 @@
 package allan.cdcar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main4Activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+    }
+    public void back(View v){
+        goBack();
+    }
+
+    private void goBack() {
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
     }
 }
