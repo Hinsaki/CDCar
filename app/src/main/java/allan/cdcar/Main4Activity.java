b73ee177951ace7c37a821348e7b0e09f5af3755
kiki4228 <john410500198@gmail.com>
2016-10-21 16:39:58 +0800

    V3

--- app/src/main/java/allan/cdcar/Main4Activity.java
+++ app/src/main/java/allan/cdcar/Main4Activity.java
@@ -1,23 +1,140 @@
 package allan.cdcar;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.ContentValues;
+import android.content.ContentResolver;
 
-public class Main4Activity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main4Activity extends Activity
+{
+    //宣告
+    private ImageView mImg;
+    private DisplayMetrics mPhone;
+    private final static int CAMERA = 66 ;
+    private final static int PHOTO = 99 ;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-    }
-    public void back(View v){
-        goBack();
+
+        //讀取手機解析度
+        mPhone = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(mPhone);
+
+        mImg = (ImageView) findViewById(R.id.img);
+        Button mCamera = (Button) findViewById(R.id.camera);
+        Button mPhoto = (Button) findViewById(R.id.photo);
+
+        mCamera.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ContentValues value = new ContentValues();
+                value.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                Uri uri= getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        value);
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri.getPath());
+                startActivityForResult(intent, CAMERA);
+            }
+        });
+
+        mPhoto.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因為點選相片後返回程式呼叫onActivityResult
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, PHOTO);
+            }
+        });
     }
 
-    private void goBack() {
-        Intent intent = new Intent(this,Main2Activity.class);
-        startActivity(intent);
+    //拍照完畢或選取圖片後呼叫此函式
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data)
+    {
+        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
+        if ((requestCode == CAMERA || requestCode == PHOTO ) && data != null)
+        {
+            //取得照片路徑uri
+            Uri uri = data.getData();
+            ContentResolver cr = this.getContentResolver();
+            String[] mMediaDATA = { MediaStore.Images.Media.DATA };
+            Cursor mCursor = managedQuery(uri,mMediaDATA,null,null,null);
+            int mImageIndex = mCursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            mCursor.moveToFirst();
+            String mPath = mCursor.getString(mImageIndex);
+            File mFile = new File(mPath);
+            if(mFile.exists()) mFile.delete();
+
+            //mPath和mFile就這現在這張圖片的路徑
+
+            try
+            {
+                //讀取照片，型態為Bitmap
+                BitmapFactory.Options mOptions = new BitmapFactory.Options();
+                //Size=2為將原始圖片縮小1/2，Size=4為1/4，以此類推
+                mOptions.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri),null,mOptions);
+
+
+                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
+                if(bitmap.getWidth()>bitmap.getHeight())ScalePic(bitmap,
+                        mPhone.heightPixels);
+                else ScalePic(bitmap,mPhone.widthPixels);
+            }
+            catch (FileNotFoundException e)
+            {
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void ScalePic(Bitmap bitmap,int phone)
+    {
+        //縮放比例預設為1
+        float mScale = 1 ;
+
+        //如果圖片寬度大於手機寬度則進行縮放，否則直接將圖片放入ImageView內
+        if(bitmap.getWidth() > phone )
+        {
+            //判斷縮放比例
+            mScale = (float)phone/(float)bitmap.getWidth();
+
+            Matrix mMat = new Matrix() ;
+            mMat.setScale(mScale, mScale);
+
+            Bitmap mScaleBitmap = Bitmap.createBitmap(bitmap,
+                    0,
+                    0,
+                    bitmap.getWidth(),
+                    bitmap.getHeight(),
+                    mMat,
+                    false);
+            mImg.setImageBitmap(mScaleBitmap);
+        }
+        else mImg.setImageBitmap(bitmap);
     }
 }
